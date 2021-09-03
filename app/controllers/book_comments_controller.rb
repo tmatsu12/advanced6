04544296954704db8fe90da60ec4e06001ed2b3d
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-03 08:45:04 +0000

    [Update]advanced3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -15,6 +15,19 @@ class BookCommentsController < ApplicationController
   end
 
   # answer
+
+#   def create
+# 		@book = Book.find(params[:book_id])
+# 		@book_comment = BookComment.new(book_comment_params)
+# 		@book_comment.book_id = @book.id
+# 		@book_comment.user_id = current_user.id
+# 		if @book_comment.save
+#   		redirect_to book_path(@book.id)
+# 		else
+# 		  render 'books/show'
+# 		end
+# 	end
+
 #   def destroy
 # 		@book = Book.find(params[:book_id])
 #   	book_comment = @book.book_comments.find(params[:id])
