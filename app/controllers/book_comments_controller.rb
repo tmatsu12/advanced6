12edf20196ebe6cdcf24fdd53ffc07b3fc993bc5
tmatsu12 <tmatsunaga65@gmail.com>
2021-09-06 06:12:01 +0000

    [Update]advanced6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,15 +2,19 @@ class BookCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+ 		@book_comment = BookComment.new(book_comment_params)
+ 		@book_comment.book_id = @book.id
+ 		@book_comment.user_id = current_user.id
+ 		@book_comment.save
+    # redirect_to request.referer
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    # BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+   	@book_comment = @book.book_comments.find(params[:id])
+ 		@book_comment.destroy
     redirect_to request.referer
   end
 
